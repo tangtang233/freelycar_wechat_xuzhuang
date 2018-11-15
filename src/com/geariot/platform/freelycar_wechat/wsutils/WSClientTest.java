9cec4cc51fb58e745227c2663f11ca76b2233450
tangtang233 <toby911115@gmail.com>
2018-11-15 16:03:43 +0800

    智能柜测试类中添加了一个开启所以柜门的方法，方便调试柜子硬件用

--- src/com/geariot/platform/freelycar_wechat/wsutils/WSClientTest.java
+++ src/com/geariot/platform/freelycar_wechat/wsutils/WSClientTest.java
@@ -1,14 +1,13 @@
 package com.geariot.platform.freelycar_wechat.wsutils;
 
 
-import com.geariot.platform.freelycar_wechat.entities.ConsumOrder;
-import com.geariot.platform.freelycar_wechat.wxutils.WechatTemplateMessage;
+import org.apache.commons.lang.StringUtils;
 
 public class WSClientTest {
     public static void main(String[] args) {
-        String deviceId = "867858031556536-10";
-        System.out.println(WSClient.getDeviceStateByID(deviceId));
-        System.out.println(WSClient.controlDevices(deviceId,"1"));
+//        String deviceId = "867858031556536-7";
+//        System.out.println(WSClient.getDeviceStateByID(deviceId));
+//        System.out.println(WSClient.controlDevices(deviceId,"1"));
 //        System.out.println(WSClient.getDeviceStateByID("867858031556536"));
 //        System.out.println(WSClient.getDeviceStateByID("867858031556536-10"));
 //
@@ -16,5 +15,19 @@ public class WSClientTest {
 //        consumOrder.setId("S20180802y92qzh");
 //        consumOrder.setState(1);
 //        WechatTemplateMessage.orderChanged(consumOrder,"ojtNs1vQCAHik8kc93vuoAKJlCzs");
+
+//        String sn = "867858031556536";
+//        openAllDoors(sn);
+    }
+
+    public static void openAllDoors(String sn) {
+        if (StringUtils.isNotEmpty(sn)) {
+            for (int i = 0; i < 16; i++) {
+                int number = i + 1;
+                String deviceId = sn + "-" + number;
+                System.out.println(WSClient.getDeviceStateByID(deviceId));
+                System.out.println(WSClient.controlDevices(deviceId, "1"));
+            }
+        }
     }
 }
